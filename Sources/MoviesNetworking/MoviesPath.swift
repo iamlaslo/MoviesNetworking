28c9f85a0e158a03cf6a0b24bfeb85f6c52c664d
Vlad Kozlov <vlad.kozlov@idapgroup.com>
2023-04-24 09:59:09 +0300

    whoops

--- Sources/MoviesNetworking/MoviesPath.swift
+++ Sources/MoviesNetworking/MoviesPath.swift
@@ -16,19 +16,9 @@ enum MoviesPath {
 
 extension MoviesPath {
   
-  static var apiBase = "https://api.themoviedb.org/3/"
-  static var developersBase = "https://developers.themoviedb.org/3/"
+  static var base = "https://api.themoviedb.org/3/"
   
   static var token = "3c1872e6a0b97d18bc554e54a774eb92"
-  
-  private var base: String {
-    switch self {
-    case .moviesList:
-      return Self.apiBase
-    case .movieDetails:
-      return Self.developersBase
-    }
-  }
   
   private var path: String {
     switch self {
@@ -48,7 +38,7 @@ extension MoviesPath {
   
   var asEndpoint: Endpoint {
     Endpoint(
-      base: self.base,
+      base: Self.base,
       path: self.path,
       method: self.method,
       query: ["api_key": Self.token]
